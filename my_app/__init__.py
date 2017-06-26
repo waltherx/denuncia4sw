682ab760b926754a12a2f3556c71fdfa5ed80130
WaltheRx <waltermj2012@gmail.com>
2017-06-26 04:10:14 -0400

    new database :=v

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:toor@localhost/denuncia'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://onfskipauhwatv:1ffb28081815308fc763ebd8187557fe4d1bfe75beb9a59c055e9e47bbf2b32e@ec2-107-21-108-204.compute-1.amazonaws.com:5432/df2dl4os00mlfj'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 from my_app.denuncia.controllers import denuncia
